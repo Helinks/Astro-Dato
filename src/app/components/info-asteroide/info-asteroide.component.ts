c422a6545567b54e36f774cdcb4a916c9d23e84e
dilansamugutierrez <dilansamugutierrez@gmail.com>
2026-09-05 13:01:18 -0500

    SE AGREGA NGIF Y NGFOR - ESTILOS NO POR QUE ME COMPLIQUE EN LA PROXIMA UPDATE SE SUBEN

--- src/app/components/info-asteroide/info-asteroide.component.ts
+++ src/app/components/info-asteroide/info-asteroide.component.ts
@@ -1,14 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuComponent}  from '../menu/menu.component';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-info-asteroide',
   templateUrl: './info-asteroide.component.html',
   styleUrls: ['./info-asteroide.component.scss'],
-  imports: [],
+  imports: [MenuComponent, NgIf],
 })
-export class InfoAsteroideComponent  implements OnInit {
+export class InfoAsteroideComponent implements OnInit {
 
-  constructor() { }
+  asteroide = {
+    nombre: 'Apophis'
+  };
+
+  constructor() {}
 
   ngOnInit() {}
 
--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { IonBackButton, IonButton } from '@ionic/angular';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss'],
-  imports: [IonBackButton, IonButton],
-})
+  imports: [IonBackButton, IonButton, NgFor],}
+)
 export class MenuComponent  implements OnInit {
 
-  constructor() { }
+  opciones = [
+    'Actividad de última misión',
+    'Crear mi testamento',
+    'Certificado de días sobrevividos'
+  ];
 
+  constructor() { }
   ngOnInit() {}
 
 }
